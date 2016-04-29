05b6d5e9a0ba1406762719201403ff9842f219fc
Alex McCool <alex.mccool@yahoo.com>
2016-04-29 10:09:58 -0700

    adding unit test from orleans source for parity

--- Tests/SimpleGrains/StreamerOutGrain.cs
+++ Tests/SimpleGrains/StreamerOutGrain.cs
@@ -1,42 +1,92 @@
-﻿using SimpleGrainInterfaces;
+﻿using Orleans;
+using Orleans.Runtime;
+using Orleans.Streams;
+using SimpleGrainInterfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SimpleGrain
 {
-    public class StreamerOutGrain : IStreamerOutGrain
+    public class StreamerOutGrain : Grain, IStreamerOutGrain
     {
+        private IAsyncStream<int> producer;
+        private int numProducedItems;
+        private IDisposable producerTimer;
+        internal Logger logger;
+        internal readonly static string RequestContextKey = "RequestContextField";
+        internal readonly static string RequestContextValue = "JustAString";
+
+        public override Task OnActivateAsync()
+        {
+            logger = base.GetLogger("StreamerOutGrain " + base.IdentityString);
+            logger.Info("OnActivateAsync");
+            numProducedItems = 0;
+            return TaskDone.Done;
+        }
+
+
         public Task BecomeProducer(Guid streamId, string streamNamespace, string providerToUse)
         {
-            throw new NotImplementedException();
+            logger.Info("BecomeProducer");
+            IStreamProvider streamProvider = base.GetStreamProvider(providerToUse);
+            producer = streamProvider.GetStream<int>(streamId, streamNamespace);
+            return TaskDone.Done;
         }
 
         public Task ClearNumberProduced()
         {
-            throw new NotImplementedException();
+            numProducedItems = 0;
+            return TaskDone.Done;
         }
 
         public Task<int> GetNumberProduced()
         {
-            throw new NotImplementedException();
+            logger.Info("GetNumberProduced {0}", numProducedItems);
+            return Task.FromResult(numProducedItems);
         }
 
         public Task Produce()
         {
-            throw new NotImplementedException();
+            return Fire();
         }
 
         public Task StartPeriodicProducing()
         {
-            throw new NotImplementedException();
+            logger.Info("StartPeriodicProducing");
+            producerTimer = base.RegisterTimer(TimerCallback, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(10));
+            return TaskDone.Done;
         }
 
         public Task StopPeriodicProducing()
         {
-            throw new NotImplementedException();
+            logger.Info("StopPeriodicProducing");
+            producerTimer.Dispose();
+            producerTimer = null;
+            return TaskDone.Done;
         }
+
+        private Task TimerCallback(object state)
+        {
+            return producerTimer != null ? Fire() : TaskDone.Done;
+        }
+
+        private async Task Fire([CallerMemberName] string caller = null)
+        {
+            RequestContext.Set(RequestContextKey, RequestContextValue);
+            await producer.OnNextAsync(numProducedItems);
+            numProducedItems++;
+            logger.Info("{0} (item={1})", caller, numProducedItems);
+        }
+
+        public override Task OnDeactivateAsync()
+        {
+            logger.Info("OnDeactivateAsync");
+            return TaskDone.Done;
+        }
+
     }
 }
